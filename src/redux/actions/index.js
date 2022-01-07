6fc730ff532d64d273891aacbd7d9dbf0abdb349
ss473901 <satoshi.473901.1205@gmail.com>
2022-01-07 16:15:00 +0900

    Add onClickEvents

---
+++ src/redux/actions/index.js
@@ -0,0 +1,40 @@
+import {
+  INPUT_NUMBER,
+  PLUS,
+  MINUS,
+  MULTIPLY,
+  DIVIDE,
+  EQUAL,
+  CLEAR,
+} from "./types";
+
+//numberがクリックされた時のイベント
+export const onNumberClick = (number) => ({
+  type: INPUT_NUMBER,
+  number,
+});
+
+// //numberをクリックした時のイベントを定義
+// 引数でnumberを受け取ってそれをpaloadとして
+//ディビューサーに渡す
+//payloadのバリューとキーが一緒の場合は省略して「number」でよい
+
+//プラスボタンが押された時のイベント
+export const onPlusClick = () => ({
+  type: PLUS,
+});
+
+//マイナスボタンがクリックされた時のイベント
+export const onMinusClick = () => ({
+  type: MINUS,
+});
+
+//マルチプレイがクリックされた時のイベント
+export const onMultiplayClick = () => ({
+  type: MULTIPLY,
+});
+
+//ディバイドがクリックされた時のイベント 
+export const onDivideClick = () => ({
+  type: DIVIDE,
+});
